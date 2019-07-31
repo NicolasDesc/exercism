4006b9cf2a05fdafe5ea7db7d169d3f9c7662d95
Nicolas Deschamps <ndeschamps@hawk.iit.edu>
2019-07-31 10:54:20 -0500

    Made dart working

--- javascript/darts/darts.js
+++ javascript/darts/darts.js
@@ -3,6 +3,14 @@
 // convenience to get you started writing code faster.
 //
 
-export const solve = () => {
-  throw new Error("Remove this statement and implement this function");
+export const solve = (x = 0, y = 0) => {
+  // Check for NaN
+  if (Number.isNaN(Number(x)) || Number.isNaN(Number(y))) return null;
+
+  const hypothenuse = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+
+  if (hypothenuse > 10) return 0;
+  if (hypothenuse > 5) return 1;
+  if (hypothenuse > 1) return 5;
+  return 10;
 };
